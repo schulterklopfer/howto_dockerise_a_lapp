f99f488a9b8c3284f9036f7869cd55db0def3d73
jash <jash@schulterklopfer-productions.de>
2018-03-26 19:46:50 +0200

    building of app is done later

--- template/images/build.sh
+++ template/images/build.sh
@@ -5,7 +5,4 @@ docker build bitcoind -t bitcoind:latest && \
 # will build lnd 0.4 beta and cherry pick some commits to support communication over docker containers
 docker build lnd -t lnd:0.4-beta && \
 # will build a base image containing the lncli command
-docker build lapp_base -t lapp_base:latest && \
-
-# will build your lightning app
-docker build lapp -t lapp:latest
+docker build lapp_base -t lapp_base:latest
