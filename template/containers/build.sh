d16522700a4ed975808032697c87fc7499d69e49
jash <jash@schulterklopfer-productions.de>
2018-03-26 11:18:10 +0200

    added some inline documentation

--- template/containers/build.sh
+++ template/containers/build.sh
@@ -1,4 +1,9 @@
+# will always build a container with the latest release of bitcoind
 docker build bitcoind -t bitcoind:latest && \
-docker build lnd -t lnd:latest -t lnd:0.4-beta && \
+# will build lnd 0.4 beta and cherry pick some commits to support communication over docker containers
+docker build lnd -t lnd:0.4-beta && \
+# will build a base image containing the lncli command
 docker build lapp_base -t lapp_base:latest && \
+
+# will build your lightning app
 docker build lapp -t lapp:latest
